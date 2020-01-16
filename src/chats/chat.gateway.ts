bf3bbcc42713f32d1775816155488643897445b5
KoaLaYT <hytohyeah@outlook.com>
2020-01-16 14:32:09 +0800

    ws test

--- src/chats/chat.gateway.ts
+++ src/chats/chat.gateway.ts
@@ -1,10 +1,19 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
+import {
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets'
+import { Server } from 'ws'
 
 @WebSocketGateway()
 export class ChatGateway {
+    @WebSocketServer()
+    server: Server
+
     @SubscribeMessage('message')
     handleMessage(client: any, payload: any): string {
-        console.log(client, payload)
+        console.log(this.server.clients)
+        console.log(typeof client, typeof payload)
         return 'Hello world!'
     }
 }
