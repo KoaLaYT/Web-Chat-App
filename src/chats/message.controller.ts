6a22e0f59d4a42b407b578dec85b0e844db05004
KoaLaYT <hytohyeah@outlook.com>
2020-01-19 23:02:49 +0800

    update server: refactor chat to message

---
+++ src/chats/message.controller.ts
@@ -0,0 +1,22 @@
+import { MessageService } from './message.service'
+import { Controller, Get, Query, Param } from '@nestjs/common'
+
+@Controller('/chat')
+export class MessageController {
+    constructor(private readonly messageService: MessageService) {}
+
+    @Get('/:userId')
+    getChat(
+        @Param('userId') userId: string,
+        @Query('with') withId: string,
+        @Query('pageSize') pageSize: string,
+        @Query('pageNum') pageNum: string,
+    ) {
+        return this.messageService.getChat({
+            u1: userId,
+            u2: withId,
+            pageSize: Number(pageSize),
+            pageNum: Number(pageNum),
+        })
+    }
+}
